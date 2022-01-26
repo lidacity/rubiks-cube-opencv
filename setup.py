f10278a92c48402c68e4bc62449b0a3428696908
dzmitry <dzmitry@lidacity.by>
2022-01-26 10:00:03 +0300

    fix

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name="rubiks_cube_opencv",
-    version="1.0.0",
+    version="1.0.1",
     description="Parse image rubiks cube to the array of colors",
     keywords="rubiks cube opencv",
     url="https://github.com/lidacity/rubiks-cube-opencv",
